99311173f844dc295fa88868a4bca95183812219
NT-Bean <samrahmani2010@gmail.com>
2025-07-01 21:34:57 -0700

    you would not BELIEVE how LONG IT TOOK ME to figure out what was wrong. i mean seriously, i literally recreated the ENTIRE THING AND I NEVER FOUNF THE GODDAMN ISSUE. WAHT THE FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
    this is why comp sci is a mistake kids

---
+++ www/html/script.js
@@ -0,0 +1,6 @@
+document.getElementById("short").addEventListener("click", function() {
+	alert("How DARE you. How fucking DARE you. You walk into MY kitchen and cook MY spaghetti using MY pot? You enter my bedroom and shit MY bed? Pathetic. It's sad to see the state of people nowadays.");
+});
+document.getElementById("long").addEventListener("click", function() {
+	alert("the other one is really long huh");
+});
