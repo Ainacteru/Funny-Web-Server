46188e7154167179eb391d4de42aa4ea26b65543
NT-Bean <samrahmani2010@gmail.com>
2025-07-02 19:49:00 -0700

    Update script.js

--- www/html/script.js
+++ www/html/script.js
@@ -1,21 +1,10 @@
 let binarySequenceWrite = false;
 let binarySequence = [];
-let keySequenceWrite = false;
-let keySequence = [];
 
 function transmitBinary(value) {
 	if (binarySequenceWrite) {
 		binarySequence[binarySequence.length] = value;
 		alert(binarySequence);
-	} else {
-		alert("it aint my job to get the voodoo backend workin, right?");
-	}
-}
-
-function transmitKey(note) {
-	if (keySequenceWrite) {
-		keySequence[keySequence.length] = note;
-		alert(keySequence);
 	} else {
 		alert("it aint my job to get the voodoo backend workin, right?");
 	}
@@ -29,15 +18,4 @@ document.getElementById("sendSeq").addEventListener("click", function() {
 	alert("insert voodoo shit idk");
 	binarySequence = [];
 	binarySequenceWrite = false;
-});
-
-
-document.getElementById("writeKeySeq").addEventListener("click", function() {
-	keySequence = [];
-	keySequenceWrite = true;
-});
-document.getElementById("sendKeySeq").addEventListener("click", function() {
-	alert("insert voodoo shit but notes idk");
-	keySequence = [];
-	keySequenceWrite = false;
 });
